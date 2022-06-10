0cbc9b41e908509c0eb436cec361eb6b667dc316
peter6053 <peternjuguna76@gmail.com>
2022-06-10 16:45:32 +0300

    complete

--- app/src/main/java/com/example/singleton/MainActivity.kt
+++ app/src/main/java/com/example/singleton/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.singleton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var ViewModel : MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        ViewModel.user.observe(this, Observer {user ->
+            println("debug ${user}")
+        })
+
+        ViewModel.SetUserId("1")
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        ViewModel.Canceljobs()
     }
 }
